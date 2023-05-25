08c4265b2b6e1eb2d9a2dd255095668e2697c753
VictorC2112 <124316196+VictorC2112@users.noreply.github.com>
2023-05-25 17:46:50 -0600

    Delete Scripts_1919410 directory

--- Scripts_1919410/bro.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Script bro.sh
-# 9/Marzo/2023 - Victor Manuel Cardenas Cavazos
-#
-read -p "Escribe tu nombre. " nombre
-echo "Hola, $nombre. Seamos amigos!"
--- Scripts_1919410/welcome.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Script welcome.sh
-# 9/Marzo/2023 - Victor Manuel Cardenas Cavazos
-#
-echo "Hola ${LOGNAME}"
-echo "Hoy es $(date)"
-echo "Usuarios actuales conectados, y sus procesos."
-w
-
